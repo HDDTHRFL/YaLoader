63589fa175dfec3d33722647306729bbe854f686
Denis Smirnov <hddthrfl@yandex.ru>
2026-06-13 23:08:47 +0300

    Fix application version in header for test

--- tests/test_app_header.py
+++ tests/test_app_header.py
@@ -4,11 +4,11 @@ from yaloader.ui.widgets.app_header import VERSION_LABEL_PREFIX, format_app_vers
 
 
 def test_format_app_version_label_adds_version_prefix() -> None:
-    assert format_app_version_label(version="1.0.0") == "v.1.0.0"
+    assert format_app_version_label(version="1.0.0") == "_ 1.0.0"
 
 
 def test_format_app_version_label_strips_spaces() -> None:
-    assert format_app_version_label(version=" 1.0.1 ") == "v.1.0.1"
+    assert format_app_version_label(version=" 1.0.1 ") == "_ 1.0.1"
 
 
 def test_format_app_version_label_handles_empty_version() -> None:
